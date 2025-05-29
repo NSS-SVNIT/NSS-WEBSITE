4e33569be1838383a0b525c861e3afc576666f69
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-05-29 16:07:47 +0530

    Date-fns fixes

--- src/utils/dateFnsAdapter.js
+++ src/utils/dateFnsAdapter.js
@@ -2,72 +2,15 @@
 // Use AdapterDateFns for date-fns v2.x compatibility
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
-// Import all required functions from our compatibility layer
-// This ensures proper named exports as expected by MUI
-import {
-	addDays,
-	addMonths,
-	addYears,
-	differenceInDays,
-	differenceInMonths,
-	differenceInYears,
-	eachDayOfInterval,
-	endOfDay,
-	endOfMonth,
-	endOfWeek,
-	endOfYear,
-	format,
-	formatDistance,
-	formatISO,
-	getDate,
-	getDaysInMonth,
-	getHours,
-	getMinutes,
-	getMonth,
-	getSeconds,
-	getWeek,
-	getYear,
-	isAfter,
-	isBefore,
-	isEqual,
-	isSameDay,
-	isSameMonth,
-	isSameYear,
-	isValid,
-	isWithinInterval,
-	parse,
-	parseISO,
-	setDate,
-	setHours,
-	setMinutes,
-	setMonth,
-	setSeconds,
-	setYear,
-	startOfDay,
-	startOfMonth,
-	startOfWeek,
-	startOfYear,
-} from "./dateFnsExports";
+// Import directly from date-fns to avoid circular dependencies
+import { format } from "date-fns";
 
 // Create a custom adapter class that extends the AdapterDateFns
 // to properly work with date-fns v2.30.0
 export class CustomDateFnsAdapter extends AdapterDateFns {
 	constructor(options) {
 		super(options);
-
-		// Override methods to use our directly imported functions
-		this.addDays = addDays;
-		this.addMonths = addMonths;
-		this.addYears = addYears;
-		this.differenceInDays = differenceInDays;
-		this.differenceInMonths = differenceInMonths;
-		this.differenceInYears = differenceInYears;
-		this.eachDayOfInterval = eachDayOfInterval;
-		this.endOfDay = endOfDay;
-		this.endOfMonth = endOfMonth;
-		this.endOfWeek = endOfWeek;
-		this.endOfYear = endOfYear;
-		// Use a custom format function to handle format string escaping properly
+		// Only override the format function to handle MUI-specific format identifiers
 		this.format = (date, formatString) => {
 			// Check if formatString is undefined or null
 			if (!formatString) {
@@ -133,39 +76,8 @@ export class CustomDateFnsAdapter extends AdapterDateFns {
 				console.warn(
 					`Unknown format string "${formatString}", using fallback format`
 				);
-				return format(date, "dd/MM/yyyy");
-			}
+				return format(date, "dd/MM/yyyy");			}
 		};
-		this.formatDistance = formatDistance;
-		this.formatISO = formatISO;
-		this.getDate = getDate;
-		this.getDaysInMonth = getDaysInMonth;
-		this.getHours = getHours;
-		this.getMinutes = getMinutes;
-		this.getMonth = getMonth;
-		this.getSeconds = getSeconds;
-		this.getWeek = getWeek;
-		this.getYear = getYear;
-		this.isAfter = isAfter;
-		this.isBefore = isBefore;
-		this.isEqual = isEqual;
-		this.isSameDay = isSameDay;
-		this.isSameMonth = isSameMonth;
-		this.isSameYear = isSameYear;
-		this.isValid = isValid;
-		this.isWithinInterval = isWithinInterval;
-		this.parse = parse;
-		this.parseISO = parseISO;
-		this.setDate = setDate;
-		this.setHours = setHours;
-		this.setMinutes = setMinutes;
-		this.setMonth = setMonth;
-		this.setSeconds = setSeconds;
-		this.setYear = setYear;
-		this.startOfDay = startOfDay;
-		this.startOfMonth = startOfMonth;
-		this.startOfWeek = startOfWeek;
-		this.startOfYear = startOfYear;
 	}
 }
 
